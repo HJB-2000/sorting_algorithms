94014d8751b0655d5021af754d749ae1fe5ffee0
HJB-2000 <jassim123bahmida@gmail.com>
2024-04-30 15:35:23 -0700

    sorting_algorithms

--- sort.h
+++ sort.h
@@ -35,4 +35,9 @@ void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+/****************103-merge_sort.c***********************/
+void *zeroalloc(unsigned int num_elements, unsigned int element_size);
+void merge_arrays(int *arr, int *tmp, int start, int mid, int end);
+void recursiveMergeSort(int *arr, int *tmp, int start, int end);
+void merge_sort(int *array, size_t size);
 #endif
