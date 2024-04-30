62a1f4848fc8673cb2adcbeaf335903f0e46cb4c
HJB-2000 <jassim123bahmida@gmail.com>
2024-04-30 07:29:22 -0700

    sorting_algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,22 +8,27 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, temp = 0;
+	size_t i = 0;
+	size_t j = 0;
+	int temp = 0;
 
 	if (size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	while (i < size)
 	{
-		for (j = 0; j < size; j++)
+		j = 0;
+		while (j < size - i - 1)
 		{
-			if (array[j] > array[j + 1] && array[j + 1])
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
 			}
+			j++;
 		}
+		i++;
 	}
 }
